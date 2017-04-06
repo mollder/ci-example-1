951996c26735f7ac05648df0199a0f3f9ee99539
mollder <mollder@naver.com>
2017-04-06 20:54:00 +0900

    commit aa

--- src/main/java/univ/lecture/Calculator.java
+++ src/main/java/univ/lecture/Calculator.java
@@ -40,19 +40,19 @@ public class Calculator{
 		double secondOperand;
 		
 		for (int i = 0; i < calTokens.length && (calTokens[i] != null); i++) {
-			if (calTokens[i].equals("*")) {
+			if ("*".equals(calTokens[i])) {
 				firstOperand = Double.parseDouble(this.stack.pop().toString());
 				secondOperand = Double.parseDouble(this.stack.pop().toString());
 				this.stack.push(firstOperand * secondOperand);
-			} else if (calTokens[i].equals("+")) {
+			} else if ("+".equals(calTokens[i])) {
 				firstOperand = Double.parseDouble(this.stack.pop().toString());
 				secondOperand = Double.parseDouble(this.stack.pop().toString());
 				this.stack.push(firstOperand + secondOperand);
-			} else if (calTokens[i].equals("/")) {
+			} else if ("/".equals(calTokens[i])) {
 				firstOperand = Double.parseDouble(this.stack.pop().toString());
 				secondOperand = Double.parseDouble(this.stack.pop().toString());
 				this.stack.push(secondOperand / firstOperand);
-			} else if (calTokens[i].equals("-")) {
+			} else if ("-".equals(calTokens[i])) {
 				firstOperand = Double.parseDouble(this.stack.pop().toString());
 				secondOperand = Double.parseDouble(this.stack.pop().toString());
 				this.stack.push(secondOperand - firstOperand);
