394c6861ba7c116fb15c9e9ccc34704b7db64187
mollder <mollder@naver.com>
2017-04-06 19:37:27 +0900

    eighth commit

--- src/test/java/univ/lecture/CalculatorTest.java
+++ src/test/java/univ/lecture/CalculatorTest.java
@@ -10,7 +10,7 @@ public class CalculatorTest {
     @Test
     public void testCalculatorAdd () {
     	Application calc = new Application();
-        int output = (int) calc.calculator("1+1");
+        double output = calc.calculator("1+1");
         assertThat(output, is(2));
     }
 }
