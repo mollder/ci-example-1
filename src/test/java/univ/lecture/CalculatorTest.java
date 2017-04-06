85abe4eba5e6f31a9c34ade9912ba2d963ff17d1
mollder <mollder@naver.com>
2017-04-06 19:22:39 +0900

    sixth commit

--- src/test/java/univ/lecture/CalculatorTest.java
+++ src/test/java/univ/lecture/CalculatorTest.java
@@ -1,13 +1,11 @@
-package univ.lecture;
+/*package univ.lecture;
 
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-/**
- * Created by tchi on 2017. 3. 19..
- */
+
 public class CalculatorTest {
     @Test
     public void testCalculatorAdd () {
@@ -15,4 +13,4 @@ public class CalculatorTest {
         int output = calc.calculate("1+1");
         assertThat(output, is(2));
     }
-}
+}*/
